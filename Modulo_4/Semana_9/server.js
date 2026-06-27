e9e4f9162197feae28408b44aa85f8fc556671ae
IsaacTOPStudent <isaacviolet1234@gmail.com>
2026-06-26 20:25:46 -0500

    Actividad semana 9

---
+++ Modulo_4/Semana_9/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send('Servidor activo y funcionando')
+})
+
+app.get('/estado', (req, res) => {
+    res.json({
+        estado: "Servidor funcionando",
+        servicio: "API comunitaria"
+    })
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Servidor ejecutandose en puerto ${PORT}`)
+})
